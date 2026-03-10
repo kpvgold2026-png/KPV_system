dfdf71679918f611bb7b70c87042a27fcc49ebb2
kpvgold2026-png <kpvgold2026@gmail.com>
2026-03-10 15:58:19 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_KEY: 'AIzaSyD9dDASdHGWQSk6ZBYOFZdhebXOXhUwpxM',
+  API_KEY: 'AIzaSyAyxKT4en6ITOeLxxeHMC5MZdkPVBN4rpI',
   SHEET_ID: '1SHbMQ9kMHvhh8mAW4Ax7UDWlEkEewQyZwrq1mTDWkkg',
   SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzBzwdETz3zjWV_zc2KA7NqKUzTBAtUk19z4bW_H1_drEUrf1QRGAOlpMDNdexp3VKQ/exec'
 };
